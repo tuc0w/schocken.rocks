4452e1b17742943ae62d9c04a73c09bc666dd42d
tuc0w <andreasbehrend@gmail.com>
2021-03-19 00:39:21 +0100

    Add games swagger definition

--- src/routes/games.js
+++ src/routes/games.js
@@ -3,7 +3,51 @@ const { Router } = require("express");
 const { GamesController } = require("../controllers");
 
 const router = Router();
+/**
+ * @swagger
+ * tags:
+ *   name: Games
+ *   description: API to manage game models.
+ *
+ * definitions:
+ *   Game:
+ *     type: object
+ *     required:
+ *       - name
+ *     properties:
+ *       id:
+ *         type: integer
+ *         description: The auto-generated id of the player.
+ *       name:
+ *         type: string
+ *         description: The name of the player.
+ *       player:
+ *         type: array
+ *         description: The players is attending this game.
+ *         items:
+ *           $ref: '#/definitions/Player'
+ *       createdAt:
+ *         type: string
+ *         format: date
+ *         description: The date of the record creation.
+ *     example:
+ *       name: Test Game
+ */
 
+/**
+ * @swagger
+ * /games:
+ *   get:
+ *     description: Retrieve a list of games.
+ *     tags: [Games]
+ *     responses:
+ *       "200":
+ *         description: All registered games.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Game'
+ */
 router.get("/", async (req, res) => {
     res.send(await GamesController.findAll());
 });
